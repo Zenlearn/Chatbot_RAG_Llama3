7f5e03a567663f7903f19cf867596b2d7dfdfa0b
Lakshya Sharma <lakshya.off31@gmail.com>
2024-12-06 21:57:26 +0530

    Fix PDF uploading error

--- pre_processing/file_processing.py
+++ pre_processing/file_processing.py
@@ -11,10 +11,12 @@ def extract_text(file: UploadFile) -> str:
     Extract text from an uploaded file (supports PDF and plain text).
     """
     if file.filename.endswith(".pdf"):
-        reader = PdfReader(file)
+        print("pdf")
+        reader = PdfReader(file.file)
         text = ""
         for page in reader.pages:
             text += " ".join(page.extract_text().replace("\n", ""))
+        print("pdf done")
         return text
 
     elif file.filename.endswith(".txt"):
